2834dc81f977f12c3ccf236a82c1d3df3acfabb9
Carlos Kruger <carloskr@yahoo.com>
2020-04-01 18:21:54 -0400

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,68 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const dotenv = require('dotenv');
+const cors = require('cors')
+const bodyParser = require("body-parser");
+
+
+dotenv.config();
+
+const app = express()
+
+app.use(cors());
+
+app.use(express.static('dist'))
+
+app.use(bodyParser.json());
+
+console.log(__dirname)
+
+var aylien = require("aylien_textapi");
+
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
+// console.log(process.env.API_ID)
+// console.log(process.env.API_KEY)
+// console.log(textapi)
+
+let aylienResponse={};
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+// app.post('/aylien', function(req,res){
+//     // textapi.sentiment({url:req.body.url,'mode':document}, (error,response)=>{
+//         textapi.sentiment({url:'https://nypost.com/2020/03/31/newhouse-family-buys-ironman-company-for-730m-cash/','mode':document}, (error,response)=>{
+//         if(error==null){
+//             aylienResponse={
+//                 "polarity":response.polarity,
+//                 "subjectivity":response.subjectivity,
+//                 "text":response.text,
+//                 "polarity_confidence":response.polarity_confidence,
+//                 "subjectivity_confidence":response.subjectivity_confidence
+//             }
+//             res.send(aylienResponse);
+//             console.log(aylienResponse);
+//         }
+//         else{
+//             console.log("Error",error);
+//         }
+//     })
+// })
+
+
